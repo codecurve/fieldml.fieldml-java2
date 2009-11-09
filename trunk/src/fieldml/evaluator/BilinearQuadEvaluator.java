1e621b4ffbe56db1406f0c5a5f4e2d3a4dec904b
caton little <caton.little@gmail.com>
2009-11-10 10:47:29 +1300

    Extracted IndirectEvaluator class to handle all interpolations based on indirect parameter mappings.

--- trunk/src/fieldml/evaluator/BilinearQuadEvaluator.java
+++ trunk/src/fieldml/evaluator/BilinearQuadEvaluator.java
@@ -1,24 +1,13 @@
 package fieldml.evaluator;
 
-import fieldml.annotations.SerializationAsString;
-import fieldml.domain.ContinuousDomain;
 import fieldml.domain.EnsembleDomain;
-import fieldml.field.MappingField;
-import fieldml.value.ContinuousDomainValue;
-import fieldml.value.EnsembleDomainValue;
-import fieldml.value.MeshDomainValue;
+import fieldml.field.ContinuousMappingField;
+import fieldml.field.EnsembleMappingField;
 
 public class BilinearQuadEvaluator
-    extends ContinuousEvaluator
+    extends IndirectEvaluator
 {
-    @SerializationAsString
-    public final MappingField<ContinuousDomain, ContinuousDomainValue> dofs;
-
-    @SerializationAsString
-    public final MappingField<EnsembleDomain, EnsembleDomainValue> dofIndexes;
-
-
-    private static double QuadBilinear( double[] params, double[] xi )
+    protected double evaluate( double[] params, double[] xi )
     {
         double p3 = xi[0] * xi[1];
         double p2 = ( 1 - xi[0] ) * xi[1];
@@ -29,32 +18,9 @@ public class BilinearQuadEvaluator
     }
 
 
-    public BilinearQuadEvaluator( String name, MappingField<ContinuousDomain, ContinuousDomainValue> dofs,
-        MappingField<EnsembleDomain, EnsembleDomainValue> dofIndexes )
+    public BilinearQuadEvaluator( String name, ContinuousMappingField dofs, EnsembleMappingField dofIndexes,
+        EnsembleDomain iteratedDomain )
     {
-        super( name );
-
-        // TODO Assert that dofIndexes value domain is dofs only parameter domain
-        // TODO Assert that dofIndexes's parameter domain has the right cardinality for the given interpolation.
-        this.dofs = dofs;
-        this.dofIndexes = dofIndexes;
-    }
-
-
-    @Override
-    public double evaluate( MeshDomainValue value )
-    {
-        final int elementIndex = value.indexValue;
-        double[] params = new double[4];
-
-        for( int i = 0; i < 4; i++ )
-        {
-            final int localNodeIndex = i + 1;
-            final EnsembleDomainValue indexOfGlobalNode = dofIndexes.evaluate( elementIndex, localNodeIndex );
-            ContinuousDomainValue dofValue = dofs.evaluate( indexOfGlobalNode );
-            params[i] = dofValue.chartValues[0];
-        }
-
-        return QuadBilinear( params, value.chartValues );
+        super( name, dofs, dofIndexes, iteratedDomain );
     }
 }
