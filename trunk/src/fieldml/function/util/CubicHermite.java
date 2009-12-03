06a6e959f7c3dd69a01eb175178c0cb85742db8a
r britten <r.britten@auckland.ac.nz>
2009-12-03 14:14:30 +1300

    Put in methods in a more sensible sequence.

--- trunk/src/fieldml/function/util/CubicHermite.java
+++ trunk/src/fieldml/function/util/CubicHermite.java
@@ -6,16 +6,16 @@ public class CubicHermite {
     return ( 1 - 3 * xi * xi + 2 * xi * xi * xi );
   }
 
-  public static double psi12(final double xi) {
-    return xi * xi * ( xi - 1 );
-  }
-
   public static double psi02(final double xi) {
     return xi * xi * ( 3 - 2 * xi );
   }
 
   public static double psi11(final double xi) {
     return xi * ( xi - 1 ) * ( xi - 1 );
+  }
+
+  public static double psi12(final double xi) {
+    return xi * xi * ( xi - 1 );
   }
 
 }
